05aa0e04a05acb8478d7b4e5de7a6e9b69088c2f
Abdullah <abdullah.akcam52@gmail.com>
2019-06-23 14:21:55 +0300

    Bug fix

--- toggle.js
+++ toggle.js
@@ -1,17 +1,13 @@
 window.onload = function () {
     function updateLabel() {
         var enabled = chrome.extension.getBackgroundPage().enabled;
-        document.getElementById('toggle-button').value = enabled ? "Off" : "On";
+        document.getElementById('toggle-button').value = enabled ? "Kapat" : "Aç";
     }
     document.getElementById('toggle-button').onclick = function () {
+        var enabled = chrome.extension.getBackgroundPage().enabled;
         var background = chrome.extension.getBackgroundPage();
         background.enabled = !background.enabled;
-        document.getElementById('on-off').innerHTML = function () {
-            if (background.enabled) {
-                return "On";
-            }
-            return "Off";
-        };
+        document.getElementById('on-off').innerHTML = enabled ? "<span id=\"disabled\">Çalışmıyor!</span>" : "Çalışıyor!";
         updateLabel();
     };
     updateLabel();
